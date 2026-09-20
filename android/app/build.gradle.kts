538c403d18a279454b263d90ed60f9dbe701a9e8
CaririApoioOperacional <caririapoio@gmail.com>
2026-09-20 02:13:29 -0300

    Cria aplicativo Android CARIRI CENTRAL 24h

---
+++ android/app/build.gradle.kts
@@ -0,0 +1,21 @@
+plugins { id("com.android.application"); id("org.jetbrains.kotlin.android") }
+android {
+ namespace="br.com.caririmonitoramento.central24h"
+ compileSdk=35
+ defaultConfig {
+  applicationId="br.com.caririmonitoramento.central24h"
+  minSdk=26
+  targetSdk=35
+  versionCode=1
+  versionName="1.0.0"
+ }
+ buildFeatures { buildConfig=true }
+ buildTypes { release { isMinifyEnabled=false; proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"),"proguard-rules.pro") } }
+}
+dependencies {
+ implementation("androidx.core:core-ktx:1.15.0")
+ implementation("androidx.appcompat:appcompat:1.7.0")
+ implementation("com.google.android.material:material:1.12.0")
+ implementation("androidx.activity:activity-ktx:1.10.0")
+ implementation("com.google.android.gms:play-services-location:21.3.0")
+}
