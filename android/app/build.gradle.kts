a7ae69ca9e6d6bc9b44b11a67fb01a18d17f1de6
CaririApoioOperacional <caririapoio@gmail.com>
2026-09-20 02:23:31 -0300

    Alinha JVM 17 na compilação Android

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -9,6 +9,8 @@ android {
   versionCode=1
   versionName="1.0.0"
  }
+ compileOptions { sourceCompatibility=JavaVersion.VERSION_17; targetCompatibility=JavaVersion.VERSION_17 }
+ kotlinOptions { jvmTarget="17" }
  buildFeatures { buildConfig=true }
  buildTypes { release { isMinifyEnabled=false; proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"),"proguard-rules.pro") } }
 }
